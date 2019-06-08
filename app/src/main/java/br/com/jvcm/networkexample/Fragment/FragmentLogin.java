4fe1abb4432f8f39e1f204a73d6667d998c48922
Joao Victor Machado <joaovcm84@gmail.com>
2019-06-08 19:23:09 -0300

    creating class FragmentLogin, fragment package, and creating layout login_activity and fragment_login

---
+++ app/src/main/java/br/com/jvcm/networkexample/Fragment/FragmentLogin.java
@@ -0,0 +1,23 @@
+package br.com.jvcm.networkexample.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import br.com.jvcm.networkexample.R;
+
+public class FragmentLogin extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_login, container, true);
+
+        return rootView;
+    }
+}
