2dc1676ca87339872141d9ad97ab044c3e6cfcff
Joao Victor Machado <joaovcm84@gmail.com>
2019-06-14 12:32:21 -0300

    creating class fragmentOffer and layout fragment_offer

---
+++ app/src/main/java/br/com/jvcm/networkexample/Fragment/FragmentOffer.java
@@ -0,0 +1,24 @@
+package br.com.jvcm.networkexample.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.zip.Inflater;
+
+import br.com.jvcm.networkexample.R;
+
+public class FragmentOffer extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_offer, container , true);
+
+        return rootView;
+    }
+}
