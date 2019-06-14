bdc0981c558f35b0050604a6e06eab20f6036833
Joao Victor Machado <joaovcm84@gmail.com>
2019-06-13 23:10:16 -0300

    Implementing button login action

--- app/src/main/java/br/com/jvcm/networkexample/LoginActivity.java
+++ app/src/main/java/br/com/jvcm/networkexample/LoginActivity.java
@@ -1,8 +1,10 @@
 package br.com.jvcm.networkexample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
@@ -15,5 +17,12 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(this::onClickLogin);
+    }
+
+    public  void onClickLogin(View view){
+        Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+
     }
 }
